a490023451ff0ef63d49419c5063c591d71a21fb
Habib Faizal Fadli <107592102+habibfaizalf@users.noreply.github.com>
2024-11-08 22:59:08 +0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-      organization = "<HABIB_FAIZAL_FADLI>"
+      organization = "HABIB_FAIZAL_FADLI"
     workspaces {
       name = "ace-iac-day-two"
     }
